2ecfe7709503268c8a38d847748d4f04c6e3ca31
regina <joyufeng@gmail.com>
2022-06-04 23:27:12 +0100

    token

--- src/token/main.mo
+++ src/token/main.mo
@@ -1,5 +1,22 @@
-actor {
-    public func greet(name : Text) : async Text {
-        return "Hello, " # name # "!";
+import Principal "mo:base/Principal";
+import HashMap "mo:base/HashMap";
+
+actor Token {
+  var owner : Principal = Principal.fromText("nxs4k-f3whc-dwwyg-hx7wq-fgyue-gvrh4-u3twp-pb2wu-cnzhc-by6hs-gqe");
+  var totalSupply : Nat = 1000000000;
+  var symbol : Text = "DANG";
+
+  var balances = HashMap.HashMap<Principal, Nat>(1, Principal.equal, Principal.hash);
+
+  balances.put(owner, totalSupply);
+
+  public query func balanceOf(who: Principal) : async Nat {
+
+    let balance : Nat = switch (balances.get(who)) {
+      case null 0;
+      case (?result) result;
     };
-};
+
+    return balance;
+  }
+}
---
+++ src/token_assets/src/components/Balance.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function Balance() {
+  const [inputValue, setInput] = useState("");
+
+  async function handleClick() {
+    // console.log(inputValue);
+    // await
+  }
+
+  return (
+    <div className="window white">
+      <label>Check account token balance:</label>
+      <p>
+        <input
+          id="balance-principal-id"
+          type="text"
+          placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </p>
+      <p className="trade-buttons">
+        <button id="btn-request-balance" onClick={handleClick}>
+          Check Balance
+        </button>
+      </p>
+      <p>This account has a balance of XYZ.</p>
+    </div>
+  );
+}
+
+export default Balance;
